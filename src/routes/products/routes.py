d53ae533a59083571a9240fcf6d37c8052082b55
jedelacarrera <jedelacarrera@uc.cl>
2020-06-08 22:46:47 -0400

    feat: :star: :sparkles: filter sales only products

--- src/routes/products/routes.py
+++ src/routes/products/routes.py
@@ -8,13 +8,17 @@ products_app = Blueprint("projects", __name__, url_prefix="/products")
 def get_products():
     search = request.args.get("search", "")
     provider_id = int("0" + request.args.get("provider", ""))
-    products = controllers.get_products(search=search, provider_id=provider_id)
+    sales_only = bool(request.args.get("sales_only"))
+    products = controllers.get_products(
+        search=search, provider_id=provider_id, sales_only=sales_only
+    )
     return render_template(
         "products.html",
         search=search,
         provider_id=provider_id,
         categories=products["categories"],
         providers=controllers.get_providers(),
+        sales_only=sales_only,
     )
 
 
